3c940d8db8e290f32867f528f7c933b5a118d888
Lightwand <karlavohra@yahoo.com>
2022-05-07 11:59:19 -0700

    Initial Commit

---
+++ M3_L2_GitHub_and_Xcode_Practice_v02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3_L2_GitHub_and_Xcode_Practice_v02
+//
+//  Created by Karla Vohra on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3_L2_GitHub_and_Xcode_Practice_v02/M3_L2_GitHub_and_Xcode_Practice_v02App.swift
@@ -0,0 +1,17 @@
+//
+//  M3_L2_GitHub_and_Xcode_Practice_v02App.swift
+//  M3_L2_GitHub_and_Xcode_Practice_v02
+//
+//  Created by Karla Vohra on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct M3_L2_GitHub_and_Xcode_Practice_v02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
